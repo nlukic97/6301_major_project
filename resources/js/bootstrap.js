356302c3e0062afbdb9db1cb0fe48a81564923ee
nlukic97 <nlukic97@gmail.com>
2021-07-02 19:04:41 +0200

    edit, doesn't work

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,7 +39,7 @@ window.Echo = new Echo({
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    forceTLS:false,
+    forceTLS:true,
     disableStats:true
 });
 
