f308c7bdbedddb0f2f262db0b4e17ff66c31823c
nlukic97 <nlukic97@gmail.com>
2021-07-04 00:58:48 +0200

    testing bootstrap.js

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -51,8 +51,8 @@ return [
             'options' => [
                 'host'   => '127.0.0.1',
                 'port'   => 6001,
-                'scheme' => 'http',
-                'useTLS' => false, // this is no error, we are talking without SSL to the WebSocket server from Laravel but your end-users will connect with SSL
+                'scheme' => 'https',
+                'useTLS' => true, // this is no error, we are talking without SSL to the WebSocket server from Laravel but your end-users will connect with SSL
             ],
         ],
 
