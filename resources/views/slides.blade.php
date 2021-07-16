271894f1e30bea2d8a6cc8b7e6b54a8f0e7c8800
nlukic97 <nlukic97@gmail.com>
2021-07-16 16:38:55 +0200

    edited the all slides section. Must sort out the last-child error

--- resources/views/slides.blade.php
+++ resources/views/slides.blade.php
@@ -12,20 +12,23 @@
                </div>
            </div>
 
-           @foreach($allSlides as $slide)
-               <div class="row border border-secondary mb-2 mt-4 pt-2 pb-2">
-                   <div class="col-10">
-                       <a class='btn btn-block text-info text-left' href="/edit-slides/{{$slide->id}}">
-                           @if($slide->title)
-                               {{$slide->title}}
-                           @else
-                               untitled
-                           @endif
-                       </a>
-                   </div>
+           @foreach($allSlides as $key=>$slide)
+               <div class="row d-flex justify-content-center" id="slides-list">
+                   <div class="col-lg-8 col-md-9 col-11 d-flex justify-content-between align-items-center pt-2 pb-2 one-slide">
 
-                   <div class="col-2 text-center">
-                       <span class='btn btn-danger float-right' href="/delete-slides/{{$slide->id}}"><i class="fas fa-times"></i></span>
+                       <div class="text-light">
+                           @if($slide->title)
+                               {{$key+1}} - {{$slide->title}}
+                           @else
+                               {{$key+1}} - untitled
+                           @endif
+                       </div>
+
+                       <div class="text-right slides-section-btns">
+                           <a class='btn btn-primary' href="/edit-slides/{{$slide->id}}"><i class="fas fa-pen"></i></a>
+                           <a class='btn btn-danger' href="/delete-slides/{{$slide->id}}"><i class="fas fa-times"></i></a>
+                       </div>
+
                    </div>
                </div>
            @endforeach
