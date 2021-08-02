e99b3009cbf68dcf9cf30ea751a71dc6b439dcbf
nlukic97 <nlukic97@gmail.com>
2021-08-02 23:43:26 +0200

    saving landing page progress

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -3,8 +3,13 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="">
-               <h1 class="text-light">Welcome to the landing page</h1>
+            <div class="col-4 text-light text-right" style="margin-top:100px">
+               <h1>Learning to <span class="text-warning">code</span> has never been this <span class="text-warning">easy.</span></h1>
+                <p class="pl-2">Slides, code, and video calls...</p>
+                <p>...all from your browser.</p>
+            </div>
+            <div class="col-6 border">
+                <img style="width: 100%;" src="https://images.unsplash.com/photo-1609751352001-25b60439239c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80" alt="">
             </div>
         </div>
     </div>
