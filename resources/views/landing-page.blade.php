09379f9e0bae02f574404adcea8c6170f546942a
nlukic97 <nlukic97@gmail.com>
2021-08-07 17:59:00 +0200

    testing the use of images in blade and css

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -9,7 +9,7 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem deserunt earum esse, ex labore molestias perspiciatis placeat recusandae sit suscipit tenetur totam voluptatum! Assumenda delectus deleniti dolore incidunt odio perspiciatis quae quaerat? Eius iste numquam officia omnis porro provident.</p>
             </div>
             <div class="col-md-6 mt-5 mb-5">
-                <img class="w-100" src="/storage/images/laptop_lesson.png" alt="">
+                <img class="w-100" src="storage/images/laptop_lesson.png" alt="">
             </div>
         </div>
     </div>
@@ -18,7 +18,7 @@
     <div class="bg-white text-dark">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-md-6 mt-5" id="hero2">
+                <div class="col-md-6" id="hero2">
                 </div>
                 <div class="col-md-6 text-dark mt-5 mb-5">
                     <h1>Learning to code has never been this easy.</h1>
