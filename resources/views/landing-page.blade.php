0374a34ea7592731f129e88bab5a9bcc7ef99101
nlukic97 <nlukic97@gmail.com>
2021-08-07 17:29:53 +0200

    completed part of the landing page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,15 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
+    {{-- ----- Section 1 ----- --}}
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-4 text-light" style="margin-top:100px">
-               <h1>Learning to <span class="text-warning">code</span> has never been this <span class="text-warning">easy.</span></h1>
-                <p class="pl-2">Slides, code, and video calls...</p>
-                <p>...all from your browser.</p>
+        <div class="row align-items-center">
+            <div class="col-md-6 text-light">
+                <h1>Learning to code has never been this easy.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem deserunt earum esse, ex labore molestias perspiciatis placeat recusandae sit suscipit tenetur totam voluptatum! Assumenda delectus deleniti dolore incidunt odio perspiciatis quae quaerat? Eius iste numquam officia omnis porro provident.</p>
             </div>
-            <div class="col-6 border">
-                <img style="width: 100%;" src="https://images.unsplash.com/photo-1609751352001-25b60439239c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80" alt="">
+            <div class="col-md-6 mt-5 mb-5">
+                <img class="w-100" src="/storage/images/laptop_lesson.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    {{-- ----- Section 2 ----- --}}
+    <div class="bg-white text-dark">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6 mt-5" id="hero2">
+                </div>
+                <div class="col-md-6 text-dark mt-5 mb-5">
+                    <h1>Learning to code has never been this easy.</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem deserunt earum esse, ex labore molestias perspiciatis placeat recusandae sit suscipit tenetur totam voluptatum! Assumenda delectus deleniti dolore incidunt odio perspiciatis quae quaerat? Eius iste numquam officia omnis porro provident.</p>
+                </div>
             </div>
         </div>
     </div>
