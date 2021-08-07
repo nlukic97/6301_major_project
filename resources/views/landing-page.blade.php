cd5797ac9cbe693d930785733e43daa008927409
nlukic97 <nlukic97@gmail.com>
2021-08-07 19:01:56 +0200

    image change

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-md-6 text-light">
-                <h1>Learning to code has never been this easy.</h1>
+                <h1>Learning to code has never been easier.</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem deserunt earum esse, ex labore molestias perspiciatis placeat recusandae sit suscipit tenetur totam voluptatum! Assumenda delectus deleniti dolore incidunt odio perspiciatis quae quaerat? Eius iste numquam officia omnis porro provident.</p>
             </div>
             <div class="col-md-6 mt-5 mb-5">
@@ -20,8 +20,8 @@
             <div class="row align-items-center">
                 <div class="col-md-6" id="hero2">
                 </div>
-                <div class="col-md-6 text-dark mt-5 mb-5">
-                    <h1>Learning to code has never been this easy.</h1>
+                <div class="col-md-6 text-dark mt-5 mb-5 pl-5">
+                    <h1>Software education accessible for everyone.</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem deserunt earum esse, ex labore molestias perspiciatis placeat recusandae sit suscipit tenetur totam voluptatum! Assumenda delectus deleniti dolore incidunt odio perspiciatis quae quaerat? Eius iste numquam officia omnis porro provident.</p>
                 </div>
             </div>
